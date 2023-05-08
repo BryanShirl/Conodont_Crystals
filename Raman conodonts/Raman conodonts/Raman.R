bb3adf7f95ec029a09c1e45261fcbf2dd1b920fa
Bukoplot <e.b.jarochowska@uu.nl>
2023-05-08 16:41:24 +0200

    Fix figure labels

--- Raman conodonts/Raman conodonts/Raman.R
+++ Raman conodonts/Raman conodonts/Raman.R
@@ -6,10 +6,10 @@ library(rlang)
 library(ggpubr)
 library(plyr)
 
-OurConodontData = read.csv("C:/Users/Bryan/Desktop/Raman/Data sets/Our raman data.csv")
-Specdata = read.csv("C:/Users/Bryan/Desktop/Raman/Data sets/Sample spectra all.csv")
-OtherAuthorConoData = read.csv("C:/Users/Bryan/Desktop/Raman/Data sets/All conodont raman data.csv")
-ThomasTeethData = read.csv("C:/Users/Bryan/Desktop/Raman/Data sets/Thomas Teeth vs Conos.csv")
+OurConodontData = read.csv("../Data sets/Our raman data.csv")
+Specdata = read.csv("../Data sets/Sample spectra all.csv")
+OtherAuthorConoData = read.csv("../Data sets/All conodont raman data.csv")
+ThomasTeethData = read.csv("../Data sets/Thomas Teeth vs Conos.csv")
 
 
 #### Chapter 1 : plots of our data ####
@@ -24,7 +24,7 @@ p = ggplot(Specdata,aes(x=Prox)) +
   geom_line(aes(y = Tripy), color = "#2686a0ff",linewidth =0.75,) +
   xlim(940,980)+
   ylim(6500,14000)+
-  xlab("Wavenumber (cm-1))") + 
+  xlab(expression("Wavenumber (cm"^-1*")")) + 
   ylab("Relative Intensity (arbitrary units)")+
   theme_classic()+
   theme(axis.text.y = element_text(angle = 90))
@@ -33,8 +33,8 @@ p = ggplot(Specdata,aes(x=Prox)) +
 p1 = ggplot(OurConodontData, aes(y=FWHM, x=Peak,color=Sample.name, shape=Sample.name)) + 
   geom_point(size=3,stroke = 2) + 
   scale_shape_manual(values=c(4,15,16,17,18,25))+
-  ylab("FWHM of v1-(PO4) peak (cm-1)") + 
-  xlab("PCMI of v1-(PO4) peak (cm-1)")+
+  ylab(expression("FWHM of v"[1]*"-(PO"[4]^-3*") peak (cm"^-1*")")) + 
+  xlab(expression("PCMI of v"[1]*"-(PO"[4]^-3*") peak (cm"^-1*")"))+
   theme_classic()+
   scale_colour_manual(values=hcl.colors(n=6, palette = "Earth"))
 
@@ -42,8 +42,8 @@ p1 = ggplot(OurConodontData, aes(y=FWHM, x=Peak,color=Sample.name, shape=Sample.
 p2 = ggplot(OurConodontData, aes(y=FWHM, x=Peak,color=CAI,shape=Sample.name)) + 
   geom_point(size=3,stroke = 2) + 
   scale_shape_manual(values=c(4,15,16,17,18,25)) + 
-  ylab("FWHM of v1-(PO4) peak (cm-1)")+ 
-  xlab("PCMI of v1-(PO4) peak (cm-1)")+
+  ylab(expression("FWHM of v"[1]*"-(PO"[4]^-3*") peak (cm"^-1*")"))+ 
+  xlab(expression("PCMI of v"[1]*"-(PO"[4]^-3*") peak (cm"^-1*")"))+
   theme_classic()+
   scale_colour_gradient(low = "#dcb37b", high = "#5e4a4b")
 
@@ -51,8 +51,8 @@ p2 = ggplot(OurConodontData, aes(y=FWHM, x=Peak,color=CAI,shape=Sample.name)) +
 p3 = ggplot(OurConodontData, aes(y=FWHM, x=Peak,color=Line,shape=Sample.name)) + 
   geom_point(size=3,stroke = 2) + 
   scale_shape_manual(values=c(4,15,16,17,18,25))+ 
-  ylab("FWHM of v1-(PO4) peak (cm-1)") + 
-  xlab("PCMI of v1-(PO4) peak (cm-1)")+
+  ylab(expression("FWHM of v"[1]*"-(PO"[4]^-3*") peak (cm"^-1*")")) + 
+  xlab(expression("PCMI of v"[1]*"-(PO"[4]^-3*") peak (cm"^-1*")"))+
   scale_colour_gradient(low = "#b6d7a8", high = "#3a4b48")+
   theme_classic()
 
@@ -68,13 +68,13 @@ ggarrange(p,p1, p2, p3,
 pa = ggplot(OtherAuthorConoData, aes(y=FWHM, x=Peak, color=Author)) + 
   geom_point(size=3) +
   theme_classic()+
- ylab("PCMI of v1-(PO4) peak (cm-1)") +
+  ylab(expression("FWHM of v"[1]*"-(PO"[4]^-3*") peak (cm"^-1*")")) +
+  xlab(expression("PCMI of v"[1]*"-(PO"[4]^-3*") peak (cm"^-1*")"))+
   theme(legend.key.size = unit(0.4, 'cm')) +
   scale_color_manual(values=c("#f1873a", "#0d455f","#fcd804", "#31b6b2"))+
   xlim(954,968)+
   ylim(0,20)+
-  theme(axis.title.x=element_blank(),
-          axis.text.x=element_blank(),
+  theme(axis.text.x=element_blank(),
           axis.ticks.x=element_blank())+
   stat_ellipse()
 
@@ -88,7 +88,8 @@ pb = ggplot(ThomasTeethData, aes(y=FWHM, x=Peak, color=Name, shape=Location
   theme(legend.key.size = unit(0.4, 'cm')) +
   geom_point(size=5) + 
   scale_shape_manual(values=c(16,15,17,18))+
-  xlab("FWHM of v1-(PO4) peak (cm-1)") + ylab("PCMI of v1-(PO4) peak (cm-1)") +
+  ylab(expression("FWHM of v"[1]*"-(PO"[4]^-3*") peak (cm"^-1*")")) + 
+  xlab(expression("PCMI of v"[1]*"-(PO"[4]*") peak (cm"^-1*")")) +
   scale_color_manual(values=c('#EBDA69', '#fcd804', 'darkgray', '#61288A', '#7D33B2', '#AE74D7', '#C59CE2', '#DCC3EE', 
                                 '#C8DDDC',  '#4C9338', '#5FB846', '#7EC76A',
                                               "#9ED58F"))+
