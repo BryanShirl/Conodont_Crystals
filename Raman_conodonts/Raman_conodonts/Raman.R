fe695b80ecd1f2f42c21c7ce9bc6cc7fa9ce2689
Bukoplot <e.b.jarochowska@uu.nl>
2023-10-07 19:56:09 +0200

    Remove spaces from filenames, rename Raman scripts referenced in the paper, remove redundant .RProj files

---
+++ Raman_conodonts/Raman_conodonts/Raman.R
@@ -0,0 +1,168 @@
+library(ggplot2)
+library(scales)
+library(rlang)
+library(ggpubr)
+library(plyr)
+library(lmodel2)
+
+OurConodontData = read.csv("../Datasets/Shirley_et_al_Raman_01.csv")
+Specdata = read.csv("../Datasets/Shirley_et_al_Raman_03.csv")
+OtherAuthorConoData = read.csv("../Datasets/Shirley_et_al_Raman_02.csv")
+ThomasTeethData = read.csv("../Datasets/Shirley_et_al_Raman_04.csv")
+
+
+#### Chapter 1 : plots of our data ####
+
+## Plots our Raman data for all 6 conodonts
+p = ggplot2::ggplot(Specdata,aes(x=Spectra)) + 
+  geom_line(aes(y = Pro..muelleri), color = "#a36b2bff",linewidth =0.75,) +
+  geom_line(aes(y = Pan..equicostatus), color = "#bf9e66ff", linewidth =0.75,) +
+  geom_line(aes(y = W..excavata), color = "#2686a0ff",linewidth =0.75,) +
+  geom_line(aes(y = B..cf..aculeatus), color = "#aecec5ff",linewidth =0.75,) +
+  geom_line(aes(y = Palmatolepis.sp.), color = "#cad5c1ff",linewidth =0.75,) +
+  geom_line(aes(y = T..gracilis), color = "#dcd1a2ff",linewidth =0.75,) +
+  xlim(940,980)+
+  ylim(6500,14000)+
+  xlab(expression("Wavenumber (cm"^-1*")")) + 
+  ylab("Relative Intensity (arbitrary units)")+
+  theme_classic()+
+  theme(axis.text.y = element_text(angle = 90))
+
+## Plots our Raman data for all 6 conodonts
+p1 = ggplot2::ggplot(OurConodontData, aes(y=FWHM, x=Peak,color=Sample.name, shape=Sample.name)) + 
+  geom_point(size=3,stroke = 2) + 
+  scale_shape_manual(values=c(4,15,16,17,18,25))+
+  ylab(expression("FWHM of ν"[1]*"-(PO"[4]^-3*") peak (cm"^-1*")")) + 
+  xlab(expression("PCMI of ν"[1]*"-(PO"[4]^-3*") peak (cm"^-1*")"))+
+  theme_classic()+
+  scale_colour_manual(values=c("#aecec5ff","#cad5c1ff","#bf9e66ff","#a36b2bff","#dcd1a2ff", "#2686a0ff"))
+
+## Plots our Raman data for all 6 conodonts (Color is CAI)
+p2 = ggplot2::ggplot(OurConodontData, aes(y=FWHM, x=Peak,color=CAI,shape=Sample.name)) + 
+  geom_point(size=3,stroke = 2) + 
+  scale_shape_manual(values=c(4,15,16,17,18,25)) + 
+  ylab(expression("FWHM of ν"[1]*"-(PO"[4]^-3*") peak (cm"^-1*")"))+ 
+  xlab(expression("PCMI of ν"[1]*"-(PO"[4]^-3*") peak (cm"^-1*")"))+
+  theme_classic()+
+  scale_colour_gradient(low = "#dcb37b", high = "#5e4a4b")
+
+## Plots our Raman data for all 6 conodonts (Color is inside to outside)
+p3 = ggplot2::ggplot(OurConodontData, aes(y=FWHM, x=Peak,color=Line,shape=Sample.name)) + 
+  geom_point(size=3,stroke = 2) + 
+  scale_shape_manual(values=c(4,15,16,17,18,25))+ 
+  ylab(expression("FWHM of ν"[1]*"-(PO"[4]^-3*") peak (cm"^-1*")")) + 
+  xlab(expression("PCMI of ν"[1]*"-(PO"[4]^-3*") peak (cm"^-1*")"))+
+  scale_colour_gradient(low = "#b6d7a8", high = "#3a4b48")+
+  theme_classic()
+
+## Combined plot of all our conodont data
+ggpubr::ggarrange(p,p1, p2, p3,
+              labels = c("a", "b", "c","d"),
+              ncol = 2, nrow = 2)
+
+#### Chapter 2: Plots of our data vs that of other authors####
+
+## Plot our data vs other publications with conodont raman data
+
+pa = ggplot2::ggplot(OtherAuthorConoData, aes(y=FWHM, x=Peak, color=Author, shape=Author)) + 
+  geom_point(size=3) +
+  theme_classic()+
+  ylab(expression("FWHM of ν"[1]*"-(PO"[4]^-3*") peak (cm"^-1*")")) +
+  xlab(expression("PCMI of ν"[1]*"-(PO"[4]^-3*") peak (cm"^-1*")"))+
+  theme(legend.key.size = unit(0.4, 'cm')) +
+  scale_shape_manual(values=c(16,15,17,18))+
+  scale_color_manual(values=c("#8babf1", "#054fb9","#c44601", "#f57600"))+
+  xlim(954,968)+
+  ylim(0,20)
+
+## Plot our data vs Thomas et.al. 2011 data (selected for enamel,enameloid and dentine)
+
+pb = ggplot2::ggplot(ThomasTeethData, aes(y=FWHM, x=Peak, color=Name, shape=Location
+ )) + 
+  theme_classic()+
+  geom_point(size=5) + 
+  scale_shape_manual(values=c(16,15,17,18))+
+  ylab(expression("FWHM of ν"[1]*"-(PO"[4]^-3*") peak (cm"^-1*")")) + 
+  xlab(expression("PCMI of ν"[1]*"-(PO"[4]*") peak (cm"^-1*")")) +
+  scale_color_manual(values=c('#EBDA69', '#fcd804', 'darkgray', '#61288A', '#7D33B2', '#AE74D7', '#C59CE2', '#DCC3EE', 
+                                '#C8DDDC',  '#4C9338', '#5FB846', '#7EC76A',
+                                              "#9ED58F"))+
+  theme(legend.key.size = unit(0.4, 'cm')) +
+  xlim(954,968)+
+  ylim(0,20)
+  
+  
+ggpubr::ggarrange(pa, pb,
+          labels = c("A", "B", "C","D"),
+          ncol = 1, nrow = 2)
+
+#### Chapter 3: Regression analysis ####
+
+models <- lapply(split(OtherAuthorConoData, OtherAuthorConoData$Author),
+       lmodel2::lmodel2, formula=FWHM ~ Peak, range.y="relative", range.x="relative",
+       nperm=99)
+
+Zhang <- models$`Zhang et al. 2017`$regression.results[models$`Zhang et al. 2017`$regression.results$Method=="RMA",]
+McMillan <- models$`McMillan & Golding 2019`$regression.results[models$`McMillan & Golding 2019`$regression.results$Method=="RMA",]
+Rantitsch <- models$`Rantitsch et al. 2020`$regression.results[models$`Rantitsch et al. 2020`$regression.results$Method=="RMA",]
+  
+ggplot2::ggplot(OtherAuthorConoData, aes(y=FWHM, x=Peak, color=Author, shape=Author)) + 
+  geom_point(size=3)+
+  theme_classic()+
+  ylab(expression("FWHM of the "*nu[1]*"-(PO"[4]^-3*") peak (cm"^-1*")")) +
+  xlab(expression("PCMI of the "*nu[1]*"-(PO"[4]^-3*") peak (cm"^-1*")"))+
+  theme(legend.key.size = unit(0.6, 'cm')) +
+  theme(legend.position = c(.85, .9), legend.text = element_text(size=12))+
+  theme(axis.text=element_text(size=12),
+        axis.title=element_text(size=14,face="bold"))+
+  scale_shape_manual(values=c(16,15,17,18))+
+  scale_color_manual(values=c("#8babf1", "#054fb9","#c44601", "#f57600"))+
+  xlim(954,968)+
+  ylim(0,20)+
+  geom_abline(intercept = McMillan$Intercept, slope = McMillan$Slope, colour = "#8babf1")+
+  geom_abline(intercept = Rantitsch$Intercept, slope = Rantitsch$Slope, colour = "#054fb9")+
+ theme(text = element_text(family = "Open Sans"))
+
+require("svglite")
+ggsave("Raman_comparison_main_text.svg")
+
+#### Exporting slope coefficients and their confidence intervals into a file ####
+
+CI_slope <- data.frame()
+for (i in 1:length(models)) {
+  CI_slope <- rbind(CI_slope, models[[i]]$confidence.intervals[4,4:5])
+}
+
+slope <- data.frame()
+for (i in 1:length(models)) {
+  slope <- rbind(slope, models[[i]]$regression.results[4,3])
+}
+colnames(slope) <- "Slope"
+
+R_squared <- data.frame()
+for (i in 1:length(models)) {
+  R_squared <- rbind(R_squared, models[[i]]$rsquare)
+}
+colnames(R_squared)<-"R_squared"
+
+
+
+Slopes_RMA_CI <- cbind(slope, CI_slope, R_squared)
+
+Slopes_RMA_CI <- sapply(Slopes_RMA_CI,
+                        FUN = round,
+                        digits = 2,
+                        simplify = T)
+Slopes_RMA_CI <- cbind(c("McMillan & Golding 2019","Rantitsch et al. 2020","Shirley et al. 2023","Zhang et al. 2017"),
+                       Slopes_RMA_CI)
+colnames(Slopes_RMA_CI) <- c("Study",colnames(CI_slope), "Slope", "R_squared")
+
+Slopes_RMA_CI <- as.data.frame(Slopes_RMA_CI)
+
+utils::write.table(Slopes_RMA_CI, 
+            file = "Slopes_RMA_CI.csv",
+            sep = ",",
+            col.names = TRUE)
+            
+
+
